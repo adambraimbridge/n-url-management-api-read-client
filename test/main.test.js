973a7b2bb943a9c8ceee31d372bd2ef245bbe4fc
Matthew Andrews <matt@mattandre.ws>
2016-02-04 15:52:59 +0000

    I have no idea what I'm doing

--- test/main.test.js
+++ test/main.test.js
@@ -3,30 +3,29 @@
 const proxyquire = require('proxyquire');
 const expect = require('chai').expect;
 
-describe('#get', () => {
-
-	before(() => {
-		const response = {
+const mockInstance = {
+	getItem: (opts, cb) => {
+		setTimeout(() => cb(null, {
 			Item: {
 				FromURL: { S: 'www.ft.com/fastft' },
 				Code: { N: '100' },
 				ToURL: { S: 'www.ft.com/stream/brandId/NTlhNzEyMzMtZjBjZi00Y2U1LTg0ODUtZWVjNmEyYmU1NzQ2-QnJhbmRz' }
 			}
-		};
-		const mockInstance = {
-			getItem: (opts, cb) => {
-				setTimeout(() => cb(null, response))
-			}
-		};
+		}))
+	}
+};
 
-		proxyquire('../lib/dynamos', {
-			master: { table: 'urlmgmtapi_master', instance: mockInstance },
-			slave: { table: 'urlmgmtapi_slave', instance: mockInstance }
-		});
-	});
+const main = proxyquire('..', {
+	'./lib/dynamos': {
+		master: { table: 'urlmgmtapi_master', instance: mockInstance },
+		slave: { table: 'urlmgmtapi_slave', instance: mockInstance }
+	}
+});
+
+describe('#get', () => {
 
 	it('should #get /fastft', () => {
-		return require('..').get('www.ft.com/fastft')
+		return main.get('www.ft.com/fastft')
 			.then(data => {
 				expect(data).to.eql({
 					code: 100,
