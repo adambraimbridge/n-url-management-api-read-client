2b9a7f7eb1314acc2781be83c540371b6705e669
Matthew Andrews <matt@mattandre.ws>
2016-02-04 17:15:06 +0000

    Test both regions out

---
+++ test/active.total-failure.test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const proxyquire = require('proxyquire');
+const expect = require('chai').expect;
+
+const mockItem = {
+	Item: {
+		FromURL: { S: 'www.ft.com/fastft' },
+		Code: { N: '100' },
+		ToURL: { S: 'www.ft.com/stream/brandId/NTlhNzEyMzMtZjBjZi00Y2U1LTg0ODUtZWVjNmEyYmU1NzQ2-QnJhbmRz' }
+	}
+};
+
+const active = proxyquire('../lib/active', {
+	'./dynamos': {
+		master: {
+			table: 'urlmgmtapi_master',
+			instance: {
+				getItem: (opts, cb) => {
+					setTimeout(() => cb(new Error("master failure")), 300)
+				}
+			}
+		},
+		slave: {
+			table: 'urlmgmtapi_slave',
+			instance: {
+				getItem: (opts, cb) => {
+					setTimeout(() => cb(new Error("slave failure")), 100)
+				}
+			}
+		}
+	}
+});
+
+describe('#active in a total failure mode', () => {
+
+	it('should start off being ‘master’', () => {
+		expect(active()).to.eql('master');
+	});
+
+	it('should just use master and hope for the best', done => {
+		setTimeout(() => {
+			expect(active()).to.eql('master');
+			done();
+		}, 500);
+	});
+
+});
