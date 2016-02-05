2cd5c8b3b36852230e6d11cd6a3bc073d0ab1dcb
Matthew Andrews <matt@mattandre.ws>
2016-02-05 11:00:01 +0000

    Pull ‘health’ out into same file

--- main.js
+++ main.js
@@ -4,24 +4,10 @@ const active = require('./lib/active');
 const get = require('./lib/get');
 const dynamos = require('./lib/dynamos');
 
+exports.health = require('./lib/health');
+
 exports.get = fromURL => {
 	const dynamo = dynamos[active()];
 	return get(dynamo.instance, dynamo.table, fromURL);
 };
 
-exports.health = opts => {
-	return {
-		getStatus: () => {
-			return {
-				name: 'n-url-management-api-read-client health check',
-				ok: !active.totalFailure,
-				checkOutput: `Total failure mode (can't reach either master or slave DynamoDBs) ${active.totalFailure}`,
-				lastUpdated: new Date(),
-				panicGuide: `Try checking the \`URLMGMTAPI_AWS_ACCESS/SECRET_KEY\`s and the status of DynamoDB in both eu-west-1 and us-east-1`,
-				severity: opts.severity,
-				businessImpact: `Lots of URLs will not work, the site will be very broken.`,
-				technicalSummary: `This app can't talk to either DynamoDB tables.`
-			}
-		}
-	};
-};
