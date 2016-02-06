f916656178cf36dde14b5c348b5d2eeda62cb3ac
Matthew Andrews <matt@mattandre.ws>
2016-02-06 10:13:02 +0000

    See what the metrics client is doing

--- test/utils/metrics-mock.js
+++ test/utils/metrics-mock.js
@@ -1,4 +1,10 @@
 'use strict';
 
-exports.count = () => {};
-exports.histogram = () => {};
+const logger = require('ft-next-logger').logger;
+
+exports.count = metric => {
+	logger.info({ event: 'METRICS_COUNT', metric });
+};
+exports.histogram = (metric, value) => {
+	logger.info({ event: 'METRICS_HISTOGRAM', metric, value });
+};
