03884eaaabdc30e74bb15e1e9ad5b43c4e5a3cc0
Matthew Andrews <matt@mattandre.ws>
2016-03-17 10:31:14 +0000

    lib/get to reject on not found

---
+++ test/lib/get.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const proxyquire = require('proxyquire');
+const expect = require('chai').expect;
+const itemFixture = require('../fixtures/fastft.json');
+const metricsMock = require('../utils/metrics-mock');
+
+const mockInstance = {
+	getItem: (opts, cb) => {
+		setTimeout(() => cb(null, {}));
+	}
+};
+
+const get = require('../../lib/get');
+
+describe('#get', () => {
+
+	it('should reject when a database doesn\'t return URL', () => {
+		return get({
+				fromURL: 'https://www.ft.com/unknown',
+				dynamo: mockInstance,
+				metrics: metricsMock,
+				table: 'urlmgmtapi_master',
+				timeout: 500
+			})
+			.then(() => {
+				throw new Error('this shouldn\'t resolve');
+			}, err => {
+				expect(err.message).to.eql('URL_NOT_FOUND');
+			});
+	});
+
+});
