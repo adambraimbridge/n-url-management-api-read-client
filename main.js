9cfea27a80af27772736229d4c6ef91c8a9dcf66
Matthew Andrews <matt@mattandre.ws>
2016-08-11 15:44:54 +0100

    feat: Add batch get

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const active = require('./lib/active');
 const get = require('./lib/get');
+const batchGet = require('./lib/batch-get');
 const dynamos = require('./lib/dynamos');
 const health = require('./lib/health');
 
@@ -29,19 +30,28 @@ exports.get = fromURL => {
 		fromURL,
 		metrics,
 		timeout
-	}).catch(err => {
-		if (err.message === 'URL_NOT_FOUND') {
-			// NB. This will still get cached by the next then because
-			// now this promise is not rejected anymore.
-			return {
-				fromURL,
-				toURL: fromURL,
-				code: 100
-			};
-		}
-		return Promise.reject(err);
-	}).then(result => {
-		return result;
+	});
+};
+
+exports.batchGet = fromURLs => {
+	// TODO: This should probably be more generic and actually parse the URL to extract the path to
+	//check that it isn't just `/` rather than being specifically for FT.com
+//	if (fromURL !== 'https://www.ft.com/' && fromURL[fromURL.length - 1] === '/') {
+//		const trimmedURL = fromURL.replace(/\/+$/, '');
+//		return Promise.resolve({
+//			fromURL,
+//			toURL: trimmedURL,
+//			code: 301
+//		});
+//	}
+
+	const dynamo = dynamos[active()];
+	return batchGet({
+		dynamo: dynamo.instance,
+		table: dynamo.table,
+		fromURLs,
+		metrics,
+		timeout
 	});
 };
 
